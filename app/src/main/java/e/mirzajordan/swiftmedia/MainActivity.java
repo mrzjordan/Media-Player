e578cc986571707328820e1ecccf2f4540ef8ef4
usman-s <43245119+usman-s@users.noreply.github.com>
2018-09-29 01:07:00 +0500

    Media Player

--- app/src/main/java/e/mirzajordan/swiftmedia/MainActivity.java
+++ app/src/main/java/e/mirzajordan/swiftmedia/MainActivity.java
@@ -1,5 +1,6 @@
 package e.mirzajordan.swiftmedia;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Doing new Panga
+
+        MediaPlayer mediaPlayer = new MediaPlayer();
     }
 }
